3924bdbd00258c385a5650aea331f0259cf6cf9d
AK <Kulkarniamogh04@icloud.com>
2025-08-28 21:04:44 +0530

    Initial Commit

---
+++ ParkChargeTests/ParkChargeTests.swift
@@ -0,0 +1,17 @@
+//
+//  ParkChargeTests.swift
+//  ParkChargeTests
+//
+//  Created by Amogh kulkarni on 28/08/25.
+//
+
+import Testing
+@testable import ParkCharge
+
+struct ParkChargeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
